4b7033ff418c406b8243e73715dd7775bd774016
JGabrielRSCorreia <gabriel.rcorreia2006@gmail.com>
2026-06-06 18:58:42 +0100

    Englished my_types.py

--- my_types.py
+++ my_types.py
@@ -1,22 +1,31 @@
+r"""
+Type aliases and generics for clarity of Markov Model related \
+implementations
+---
+
+Aliases defined:
+
+"""
+
 import numpy as np
 from typing import *
 from collections.abc import Sequence
 
 V = TypeVar("V")
-StateT = TypeVar("StateT")  # tipo dos nomes dos estados
-ValueT = TypeVar("ValueT")  # tipo dos nomes dos valores
-LabelT = TypeVar("LabelT")  # tipo das labels de emissão
+StateT = TypeVar("StateT")  # states' names' type
+ValueT = TypeVar("ValueT")  # values' names' type
+LabelT = TypeVar("LabelT")  # emission labels' type
 
-Time     = int   # T: comprimento da cadeia
-NStates  = int   # N: nº de estados
-NValues  = int   # M: nº de valores possíveis para as observações
+Time     = int   # T: sequence length
+NStates  = int   # N: no. of states
+NValues  = int   # M: no. of possible values for the observations
 
-                            # dimensões das matrizes    # tipo de dados (quase todas guardam probabilidades - floats)
+                                        # matrix dimensions    # data type (almost all store probebilities - floats)
 type Vector1D[V]    = np.ndarray[tuple[NStates                  ], V]
 type Matrix2D[V]    = np.ndarray[tuple[NStates, NStates         ], V]
 type Emission2D[V]  = np.ndarray[tuple[NStates, NValues         ], V]
 
-type AlgArray2D[V]  = np.ndarray[tuple[Time   , NStates         ], V] # matriz psi guarda índices de estados - ints
+type AlgArray2D[V]  = np.ndarray[tuple[Time   , NStates         ], V] # psi matrix stores state indices - ints
 type AlgArray3D[V]  = np.ndarray[tuple[Time   , NStates, NStates], V]
 
-type SeqInput[V] = Sequence[V] | Sequence[int]
+type SeqInput[V] = Sequence[V] | Sequence[int]      # sequences can be referenced by the names or their indices
